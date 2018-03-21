bd62c5731262c466ebbb9fe7d4b5fa4d9683a324
Rohan Panda <rohanpanda@Rohans-MacBook-Air.local>
2018-03-21 18:18:25 +0530

    Triangle and Diamond

--- src/Main.purs
+++ src/Main.purs
@@ -10,13 +10,6 @@ import Data.Maybe (Maybe(..))
 import Partial.Unsafe (unsafePartial)
 import Point (drawPoint)
 
-recursion :: Number -> Number -> String -> Array Record
-recursion x y z =if y/=500.0 
-  then (if x/=500.0 
-    then [{x: x, y:y, color:z}] <> recursion (x+10.0) y z 
-    else [] <> recursion 100.0 (y + 10.0) z) 
-  else []
-
 type Record = {
   x :: Number,
   y :: Number,
@@ -24,8 +17,55 @@ type Record = {
 
 }
 
-matrix :: Array Record
-matrix = recursion 100.0 100.0 "#ff0000"
+-- Rectangle Logic
+
+rectLines :: Number -> Number -> String -> Array Record
+rectLines x y z = if y/=500.0 
+  then (if x/=500.0 
+    then [{x: x, y:y, color:z}] <> rectLines (x+10.0) y z 
+    else [] <> rectLines 100.0 (y + 10.0) z) 
+  else []
+
+rectMatrix :: Array Record
+rectMatrix = rectLines 100.0 100.0 "#ff0000"
+
+drawFilledRectangle ctx = traverse (drawPoint ctx) rectMatrix
+
+-- Triangle Logic
+
+triLines :: Number -> Number -> String -> String -> Array Record
+triLines x y z dir = if y/=800.0 && dir == "down"
+  then ([{x: x, y:y, color:z}] <> triLines (x-10.0) (y+10.0) z dir)
+  else (if (x/=500.0 && dir == "down")
+    then ([{x: x, y:y, color:z}] <> triLines (x+10.0) y z dir)
+    else if y/=600.0
+      then [{x: x, y:y, color:z}] <> triLines (x-10.0) (y-10.0) z "up"
+    else [])
+
+triMatrix :: Array Record
+triMatrix =  triLines 300.0 600.0 "#ff0000" "down"
+
+drawTriangle ctx = do
+  traverse (drawPoint ctx) triMatrix
+
+-- Diamond Logic
+
+diaLines :: Number -> Number -> String -> String -> Array Record
+diaLines x y z dir = if y<1200.0 && dir == "down"
+  then ([{x: x, y:y, color:z}] <> diaLines (x-10.0) (y+10.0) z dir)
+  else if (x<300.0 && dir == "down")
+    then ([{x: x, y:y, color:z}] <> diaLines (x+10.0) (y+10.0) z dir)
+    else if x<500.0 && dir /= "up-right"
+      then [{x: x, y:y, color:z}] <> diaLines (x+10.0) (y-10.0) z "up-down"
+      else if y/=1000.0 
+        then [{x: x, y:y, color:z}] <> diaLines (x-10.0) (y-10.0) z "up-right"
+      else []
+
+diaMatrix :: Array Record
+diaMatrix =  diaLines 300.0 1000.0 "#ff0000" "down"
+
+drawDiamond ctx = do
+  traverse (drawPoint ctx) diaMatrix
 
 
 main :: forall e. Eff (canvas :: CANVAS | e) Unit
@@ -33,6 +73,13 @@ main = void $ unsafePartial do
   Just canvas <- getCanvasElementById "canvas"
   ctx <- getContext2D canvas
 
-  
-  traverse (drawPoint ctx) matrix
-  -- pure unit 
+  -- Draw Rectangle
+  _ <- drawFilledRectangle ctx
+
+  -- Draw Triangle
+  _ <- drawTriangle ctx
+
+    -- Draw Diamond
+  _ <- drawDiamond ctx
+
+  pure unit 
